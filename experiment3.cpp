ec3c5477da0ca744070d667c1b80e750703efab7
ataraxia <mhaskeshivraj14@gmail.com>
2026-04-06 14:27:43 +0530

    Update experiment3.cpp

--- experiment3.cpp
+++ experiment3.cpp
@@ -1,61 +1,58 @@
-#include <iostream> // Header file for input and output operations
-using namespace std; // Allows use of standard library names without std::
-// Class definition
-class Patient
+#include <iostream>
+using namespace std;
+
+class HospitalPatient
 {
-private: 
-int patientId; 
-string patientName;
-float billAmount; 
-string appointmentDate; 
+private:
+    int id;
+    string name;
+    double amount;
+    string date;
+
 public:
+    HospitalPatient()
+    {
+        id = -1;
+        name = "No Name";
+        amount = 0;
+        date = "N/A";
+    }
+    HospitalPatient(int pid, string pname, double bill, string appDate)
+    {
+        id = pid;
+        name = pname;
+        amount = bill;
+        date = appDate;
+    }
 
-Patient()
-{
-patientId = 0; 
-patientName = "Not Assigned"; 
-billAmount = 0.0; 
-appointmentDate = "Not Fixed";
-}
-// Parameterized Constructor
-Patient(int id, string name, float bill, string date)
-{
-patientId = id; // Assign value to patient ID
-patientName = name; // Assign value to patient name
-billAmount = bill; // Assign value to bill amount
-appointmentDate = date;// Assign value to appointment date
-}
-// Copy Constructor
-Patient(Patient &p)
-{
-patientId = p.patientId; 
-patientName = p.patientName;
-billAmount = p.billAmount; 
-appointmentDate = p.appointmentDate;
-}
+    HospitalPatient(const HospitalPatient &obj)
+    {
+        id = obj.id;
+        name = obj.name;
+        amount = obj.amount;
+        date = obj.date;
+    }
 
-void display()
-{
-cout << "\nPatient ID : " << patientId; 
-cout << "\nPatient Name : " << patientName; 
-cout << "\nBilling Amount : Rs. " << billAmount; 
-cout << "\nAppointment Date : " << appointmentDate; 
-cout << endl; 
-}
+    void show()
+    {
+        cout << "\nPatient ID: " << id;
+        cout << "\nName: " << name;
+        cout << "\nBill: Rs. " << amount;
+        cout << "\nAppointment: " << date << endl;
+    }
 };
 
 int main()
 {
-Patient p1;
-cout << "\n--- Default Constructor ---";
-p1.display(); 
+    HospitalPatient a;
+    cout << "\n--- Default Details ---";
+    a.show();
+    HospitalPatient b(101, "Rahul Patil", 3500.50, "15-02-2026");
+    cout << "\n--- Given Details ---";
+    b.show();
+    HospitalPatient c(b);
+    cout << "\n--- Copied Details ---";
+    c.show();
 
-Patient p2(101, "Rahul Patil", 3500.50, "15-02-2026");
-cout << "\n--- Parameterized Constructor ---";
-p2.display(); 
-
-Patient p3 = p2;
-cout << "\n--- Copy Constructor ---";
-p3.display();
-return 0; 
+    return 0;
 }
