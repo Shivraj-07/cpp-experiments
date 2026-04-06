f69a141c66a0bfc6d38ba4fdbf4714a3caa2586d
ataraxia <mhaskeshivraj14@gmail.com>
2026-04-06 14:29:16 +0530

    Update experiment5.cpp

--- experiment5.cpp
+++ experiment5.cpp
@@ -1,32 +1,36 @@
-#include <iostream> // Header file for input and output
+#include <iostream>
 using namespace std;
-// Class definition
-class Student
+
+class Learner
 {
 private:
- static int count; // Static data member (common for all objects)
+    static int totalStudents;
+
 public:
- // Constructor
- Student()
- {
- count++; // Increment count whenever object is created
- }
- // Static member function
- static void showCount()
- {
- cout << "Total number of students: " << count << endl;
- // Static function can access only static data members
- }
+    // Constructor increments count
+    Learner()
+    {
+        totalStudents++;
+    }
+
+    // Static function to display count
+    static void displayTotal()
+    {
+        cout << "Number of student objects created: " << totalStudents << endl;
+    }
 };
-// Initialization of static data member
-int Student::count = 0;
-// Main function
+
+// Initialize static variable
+int Learner::totalStudents = 0;
+
 int main()
 {
- Student s1; // Create first object
- Student s2; // Create second object
- Student s3; // Create third object
- // Call static member function using class name
- Student::showCount();
- return 0; // End of program
+    Learner obj1;
+    Learner obj2;
+    Learner obj3;
+
+    // Calling static function
+    Learner::displayTotal();
+
+    return 0;
 }
